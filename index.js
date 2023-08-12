db8652513bd8bed6b83abac7b5b0d3857c7ecbeb
Brianna Ponder <lemleybrianna123100@gmail.com>
2023-08-12 15:50:24 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, str) {
+    const lowerCaseString = str.toLowerCase()
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase().includes(lowerCaseString))
+    return matchingDrivers
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(name => name.startsWith(string))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+}
